9a991c4634e8325e0fc96b1b2adbfc8fe7d339a0
snehal4238 <55576067+snehal4238@users.noreply.github.com>
2022-03-02 22:30:43 +0530

    Update positiveno.py

--- positiveno.py
+++ positiveno.py
@@ -1,9 +1,9 @@
 list1=[12,-7,5,64,-14]
 list2=[12,14,-95,3]
-nl=[n for n in list1 if n>0]
-print(nl)
-for x in list2:
+
+for x in list1: 
     if x>0:
         print(x)
 
-
+nl=[n for n in list2 if n>0] #to print in list method []*
+print(nl)
